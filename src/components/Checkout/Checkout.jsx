444d5f4d27307d3d609e39d37cc1b9d6def9650a
Carlos <carlosborrerollinas@gmail.com>
2023-07-06 22:54:29 -0500

    Creación de ordenes implementada

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,22 +1,75 @@
 
-
-
+import { addDoc, collection, query, where, documentId, getDocs, writeBatch } from "firebase/firestore";
+import { useCart } from "../../context/CartContext";
+import { useNavigate } from "react-router-dom";
+import { db } from "../../services/firebase/firebaseConfig";
 
 const Checkout = () => {
 
-    
+    const {cart, total} = useCart()
+    const navigate = useNavigate()
 
-    const createOrder = () => {
+    const createOrder = async () => {
         const objOrder = {
             buyer: {
                 name: "Carlos Borrero",
                 phone: "123456",
                 email: "asd@asd.com"
             },
-            items: {
-                id: 
-            }
+            items: cart,
+            total
         }
+
+        try{
+            const ids = cart.map(prod => prod.id)
+            const productsRef = query(collection (db, "products"), where(documentId(), "in", ids))
+
+            // getDocs(productsRef).then((querySnapshot) => {
+
+            // })
+
+            const { docs } = await getDocs(productsRef)
+
+            const batch = writeBatch(db)
+
+            const outOfStock =[]
+
+            docs.forEach(doc => {
+                const fieldsDoc = doc.data()
+                const stockDb = fieldsDoc.stock
+
+                const productAddedToCart = cart.find(prod => prod.id === doc.id)
+                const prodQuantity = productAddedToCart?.quantity
+
+                if(stockDb >= prodQuantity){
+                    batch.update(doc.ref, {stock:stockDb-prodQuantity})
+                }else{
+                    outOfStock.push({id: doc.id, ...fieldsDoc})
+                }
+            })
+
+            if(outOfStock.length === 0) {
+                batch.commit()
+
+                const ordersRef = collection(db, "orders")
+
+                const {id} = await addDoc(ordersRef, objOrder)
+
+                // console.log("la compra fue realizada, id: " + id)
+
+                navigate("/")
+
+            }
+        }catch(error){
+            console.log(error)
+        }
+
+
+        // const db =getFirestore();
+
+        // const objectCollection = collection(db, "orders")
+
+        // addDoc(objectCollection, objOrder).then(({id}) => setObjectId(id));
     }
 
 
